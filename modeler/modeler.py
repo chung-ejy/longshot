2f6d5975333af587617761c45e08d5cc0f5c6da4
chung-ejy <chung.ejy@gmail.com>
2021-10-04 19:22:42 +0900

    implemented additional apis

--- modeler/modeler.py
+++ modeler/modeler.py
@@ -13,6 +13,10 @@ import pandas as pd
 import xgboost as xgb
 from xgboost.sklearn import XGBClassifier
 from xgboost.training import train
+from catboost import CatBoostRegressor
+from lightgbm import LGBMRegressor
+import warnings
+warnings.filterwarnings(action='ignore')
 
 class Modeler(object):
 
@@ -49,9 +53,38 @@ class Modeler(object):
         try:
             params = {"booster":["gbtree","gblinear","dart"]}
             X_train, X_test, y_train, y_test = self.shuffle_split(data)
-            gs = GridSearchCV(xgb.XGBRegressor(objective="reg:squarederror"),param_grid=params,scoring="r2")
+            gs = GridSearchCV(xgb.XGBRegressor(objective="reg:squarederror",verbosity = 0),param_grid=params,scoring="r2")
             gs.fit(X_train,y_train)
-            score = r2_score(gs.predict(X_test),y_test)
+            predictions = gs.predict(X_test)
+            score = r2_score(predictions,y_test)
+            model = gs.best_estimator_
+            return {"api":"xgb","model":model,"score":score}
+        except Exception as e:
+            print(str(e))
+    
+    @classmethod
+    def light_regression(self,data):
+        try:
+            params = {"boosting_type":["gbdt","goss","dart","rf"]}
+            X_train, X_test, y_train, y_test = self.shuffle_split(data)
+            gs = GridSearchCV(LGBMRegressor(verbosity = 0),param_grid=params,scoring="r2")
+            gs.fit(X_train,y_train)
+            predictions = gs.predict(X_test)
+            score = r2_score(predictions,y_test)
+            model = gs.best_estimator_
+            return {"api":"xgb","model":model,"score":score}
+        except Exception as e:
+            print(str(e))
+    
+    @classmethod
+    def cat_regression(self,data):
+        try:
+            params = {"boosting_type":["Ordered","Plain"]}
+            X_train, X_test, y_train, y_test = self.shuffle_split(data)
+            gs = GridSearchCV(CatBoostRegressor(objective="mape",verbosity = 0),param_grid=params,scoring="r2")
+            gs.fit(X_train,y_train)
+            predictions = gs.predict(X_test)
+            score = r2_score(predictions,y_test)
             model = gs.best_estimator_
             return {"api":"xgb","model":model,"score":score}
         except Exception as e:
